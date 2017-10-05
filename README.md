ef5572638bbc532c5fd41739a152e25681e3d19f
Alexandru Nedelcu <noreply@alexn.org>
2017-10-05 18:30:11 +0300

    Enable Travis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Monix.io
 
 The homepage and the documentation website for the Monix project.
+
+[![Travis](https://img.shields.io/travis/monix/monix.io.svg)](https://travis-ci.org/monix/monix.io)
---
+++ scripts/build-then-sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`/..
+
+# Cleanup
+rm -rf docs && rm -rf _site
+
+# Compiling tut files
+sbt -mem 4096 run
+
+# Compiling Jekyll website
+bundle exec jekyll build
+
+# Copying SSH key
+mkdir -p $HOME/.ssh
+cp scripts/travis_rsa $HOME/.ssh
+chmod -R go-rwx $HOME/.ssh
+
+rsync --delete-excluded -Pacv \
+  -e "ssh -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
+  _site/ alex@alexn.org:/var/www/monix.io/
