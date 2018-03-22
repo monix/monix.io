9deb96b77171c27ce83780d1f318e83521d14620
Alexandru Nedelcu <noreply@alexn.org>
2018-03-22 09:10:04 +0200

    Fix rsync

--- scripts/build-then-sync.sh
+++ scripts/build-then-sync.sh
@@ -16,5 +16,5 @@ cp scripts/travis_rsa $HOME/.ssh
 chmod -R go-rwx $HOME/.ssh
 
 rsync --delete-excluded -Pacv \
-  -e "ssh -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
+  -e "ssh -p 223 -o 'StrictHostKeyChecking no' -i $HOME/.ssh/travis_rsa" \
   _site/ alex@alexn.org:/var/www/monix.io/
