91387c85f9a614c9372ec7518b1bdaf5b52439ea
Alexandru Nedelcu <alexandru@users.noreply.github.com>
2020-04-22 18:12:24 +0300

    Update Twitter/YouTube card tags, remove dependencies, add Analytics (#90)
    * Update Twitter/YouTube card tags, remove dependencies, add Analytics
    
    * Remove junk
    
    * Update bundler

--- _plugins/video_links.rb
+++ _plugins/video_links.rb
@@ -1,5 +1,9 @@
+require 'net/http'
+
 module Jekyll
   module VideoLinks
+    @@_vimeo_cache = {}
+
     def vimeo_link(uid)
       "https://vimeo.com/#{uid}"
     end
@@ -8,16 +12,16 @@ module Jekyll
       "https://player.vimeo.com/video/#{uid}?title=0&byline=0&portrait=0&dnt=1"
     end
 
-    def vimeo_raw_link(uid)
-      "https://videos.monix.io/redirect/#{uid}/HD%201080p?download=false&fallback=true"
+    def vimeo_thumb_link(uid, min_width=1200)
+      get_vimeo_thumb_link(uid, min_width, false)
     end
 
-    def vimeo_thumb_link(uid)
-      "https://videos.monix.io/thumb/#{uid}"
+    def vimeo_thumb_play_link(uid, min_width=1200)
+      get_vimeo_thumb_link(uid, min_width, true)
     end
 
-    def vimeo_thumb_play_link(uid)
-      "https://videos.monix.io/thumb-play/#{uid}"
+    def youtube_thumb_link(uid, min_width=1200)
+      "https://img.youtube.com/vi/#{uid}/maxresdefault.jpg"
     end
 
     def youtube_link(uid)
@@ -27,8 +31,61 @@ module Jekyll
     def youtube_player_link(uid)
       "https://www.youtube-nocookie.com/embed/#{uid}"
     end
+
+    def warn(msg)
+      if defined? Jekyll
+        Jekyll.logger.warn(msg)
+      else
+        STDERR.puts msg
+      end
+    end
+    
+    def get_vimeo_thumb_link(uid, min_width=1200, with_play=false)
+      key = "#{uid}-#{min_width}-#{with_play}"
+      if @@_vimeo_cache.has_key? key
+        return @@_vimeo_cache[key]
+      end
+
+      Jekyll.logger.info("             Vimeo: Fetching thumb link for #{uid}")
+      link = get_vimeo_thumb_link_raw(uid, min_width, with_play)
+      @@_vimeo_cache[key] = link
+      return link
+    end
+
+    def get_vimeo_thumb_link_raw(uid, min_width=1200, with_play=false)
+      unless ENV['VIMEO_API_TOKEN']
+        warn("WARNING — VIMEO_API_TOKEN not set, querying low-resolution thumbnail for https://vimeo.com/#{uid}")
+        response = Net::HTTP.get URI("https://vimeo.com/api/v2/video/#{uid}.json")
+        json = JSON.parse(response)
+        if json.length > 0 
+          return json[0]['thumbnail_large']
+        end
+      else
+        response = Net::HTTP.get URI("https://api.vimeo.com/videos/#{uid}/pictures/?access_token=#{ENV['VIMEO_API_TOKEN']}&per_page=100")
+        json = JSON.parse(response)
+        sizes = begin
+          json['data'][0]['sizes'].sort_by {|x| x['width'] }
+        rescue
+          []
+        end
+    
+        elem = sizes.detect {|x| x['width'] >= min_width }
+        unless elem
+          elem = sizes.last
+        end
+    
+        if elem
+          if with_play
+            return elem['link_with_play_button']
+          else
+            return elem['link']
+          end
+        end
+    end
+      warn("WARNING — couldn't find thumbnail for video: https://vimeo.com/#{uid}")
+      return nil
+    end
   end
 end
 
 Liquid::Template.register_filter(Jekyll::VideoLinks)
-
--- public/js/init.js
+++ public/js/init.js
@@ -1,45 +1,3 @@
-function initVimeo(uid) {
-  $(document).ready(function () {
-    // Building the list
-    function add(list, item) {
-      var kind = encodeURIComponent(item.public_name.toLowerCase())
-      list.append(
-        "<li>" +
-        "<a href='https://videos.monix.io/redirect/" + uid + "/" + kind + "/?download=false' target='_blank' rel='nofollow'>" +
-        item.public_name + "</a> (" + item.size + ")" +
-        "</li>"
-      );
-    }
-
-    $.ajax({
-      url: "https://videos.monix.io/get/" + uid,
-      dataType: 'json',
-      success: function (data) {
-        if (data.allow_downloads) {
-          var list = $("#video-download > ul");
-          var added = false;
-
-          if (data.files) {
-            for (var i = 0; i < data.files.length; i++) {
-              var item = data.files[i];
-              add(list, item);
-              added = true;
-            }
-          }
-          if (data.source_file) {
-            add(list, data.source_file);
-            added = true;
-          }
-
-          if (added) {
-            $("#video-download").show();
-          }
-        }
-      }
-    });
-  });
-}
-
 $(document).ready(function() {
   $('#toc').toc({
     title: '',
@@ -52,23 +10,4 @@ $(document).ready(function() {
   if (hash) {
     window.location.hash = hash;
   }
-
-  // ------------------------------------------------------------------------
-
-  function vimeoTriggerVideo() {
-    var playButton = $('#vimeo .play-button');
-    var src = playButton.attr('data-src');
-    var ratio = playButton.attr('data-ratio');
-
-    $('#vimeo').html(
-      '<div id="video-frame" class="presentation" style="padding-bottom: ' + ratio + '%;">\n' +
-      '  <iframe src="' + src + '&autoplay=1" id="vimeo-iframe" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; fullscreen"></iframe>\n' +
-      '</div>'
-    );
-  }
-
-  $('#vimeo a').click(function (e) {
-    e.preventDefault();
-    vimeoTriggerVideo();
-  });
 });
